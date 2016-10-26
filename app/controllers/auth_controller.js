cf091e37fce4c6407549948b72a958eb2b76ff72
dylanjsa90 <dylanjsanders1@gmail.com>
2016-10-25 23:12:10 -0700

    added base scss file, bootstrap and font-awesome vendor files, and signup component

---
+++ app/controllers/auth_controller.js
@@ -0,0 +1,35 @@
+'use strict';
+
+module.exports = (app) => {
+  app.controller('AuthController', ['$http', '$location', '$window', 'Auth', function($http, $location, $window, Auth) {
+    if (Auth.getToken({noRedirect: true})) $location.path('/home');
+
+    this.signup = function(user) {
+      $http.post(this.baseUrl + '/api/signup', user)
+        .then(res => {
+          Auth.setToken(res.data.token);
+          $location.path('/home');
+        }, (err) => {
+          alert('Sorry, username already exists');
+        });
+    };
+
+    this.signin = function(user)  {
+      $http.get(this.baseUrl + '/api/signin', {
+        headers: {
+          'Authorization': 'Basic ' + $window.btoa(user.email + ':' + user.password)
+        }
+      })
+      .then(res => {
+        Auth.setToken(res.data.token);
+        $location.path('/home');
+      }, (err) => {
+        alert('Invalid Login Credentials');
+      });
+    };
+
+    this.user = Auth.getUser();
+    this.logOut = Auth.logOut.bind(Auth);
+
+  }]);
+};
