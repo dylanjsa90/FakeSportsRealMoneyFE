873b5682a07863103006bcd6dd73ead7278696ad
dylanjsa90 <dylanjsanders1@gmail.com>
2016-10-26 21:32:33 -0700

    added footer, navbar, started main component and sign in and out html pages

--- app/components/index.js
+++ app/components/index.js
@@ -3,4 +3,7 @@
 module.exports = (app) => {
   require('./sign_up')(app);
   require('./sign_in')(app);
+  require('./nav_bar')(app);
+  require('./footer_component')(app);
+  require('./main')(app);
 };
